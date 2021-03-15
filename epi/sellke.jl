9ca15354b9920c636fce87869228c25785f62ad4
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-03-14 17:22:59 -0700

    Sellke w/out time

--- epi/sellke.jl
+++ epi/sellke.jl
@@ -4,6 +4,7 @@ using Distributions
 m = 5 # I₀ 
 n = 1000 # S₀
 γ = 1/5
+β = 1/5 * 0.015
 
 make_infection = function(dist, x...)
     return ()->rand(dist(x...))
@@ -14,5 +15,14 @@ infection = make_infection(Exponential,1/5)
 Q = [rand(Exponential()) for i=1:n]
 sort!(Q)
 
-T0 = [infection(γ) for i=1:m]
-T = [infection(γ) for i=1:n]
+T₀ = [infection() for i=1:m]
+T = [infection() for i=1:n]
+
+Λ = Vector{Float64}()
+push!(Λ, sum(T₀) * β)
+append!(Λ, (cumsum(T[1:end-1]) * β) .+ Λ )
+
+Z = Q .> Λ
+
+# Z is the number of individuals who escape infection
+sum(Z)
